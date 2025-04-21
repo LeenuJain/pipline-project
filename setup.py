ab016db5db654e95184023210c237d424a181798
LeenuJain <141702372+LeenuJain@users.noreply.github.com>
2025-04-21 12:38:01 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="my-python-project",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.6",
+)
